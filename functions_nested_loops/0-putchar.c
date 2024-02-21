654685e279ee1b50cbae934651c3a25e733efb46
Magnusmajo <alexisoblivion@gmail.com>
2024-02-21 19:09:37 +0000

    0. _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * main - prints _putchar, followed by a new line
- * Return: 0 on success
- */
+* main - imprime _putchar, seguido de un salto de línea
+* Return: 0 en caso de éxito
+*/
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
-
