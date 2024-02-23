3d96437dfb16b9729ee31a796bb5ea868bd9e681
root <alexisoblivion.com>
2024-02-23 14:01:41 +0000

    new

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,11 @@
+/*
+ * File: 0-reset_to_98.c
+ * Author: Alexis Rodriguez
+ */
 #include "main.h"
-
 /**
- * reset_to_98 a pointer to an int as parameter
- * and updates the value it points to to 98.
+ * reset_to_98 - Takes a pointer to an int and updates
+ *               the value it points to to 98.
  * @n: The pointer to an int.
  */
 void reset_to_98(int *n)
