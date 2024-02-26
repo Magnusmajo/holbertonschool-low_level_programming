f025fa361584bbb6d0e2c4fb83d77f2421c3b728
root <alexisoblivion@gmail.com>
2024-02-26 18:38:49 +0000

    4. a function that prints a string, in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+/*
+ * File 4-print_rev.c
+ * Author: Alexis R. Rodriguez
+ */
+#include "main.h"
+/**
+  * print_rev - prints a string in reverse
+  * @s: that's the string
+  *
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	int count = 0;
+	for (; *s;)
+	{
+		count++;
+		s++;
+	}
+	for (; count; count--)
+	{
+		s--;
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
