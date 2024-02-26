962c1297d10bc56ecb84fc6ff8c984973fea86fe
root <alexisoblivion@gmail.com>
2024-02-26 18:26:29 +0000

    3. I do not fear computers. I fear the lack of them

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+/*
+ * File: 3-puts.c
+ * Author: Alexis R. Rodriguez
+ */
+#include "main.h"
+/**
+  * _puts - prints a string to standard output
+  * @str: is the string
+  *
+  * Return: is void
+  */
+void _puts(char *str)
+{
+	while (*str != 0)
+	{
+		_putchar(*str);
+		&*str++;
+	}
+	_putchar('\n');
+}
