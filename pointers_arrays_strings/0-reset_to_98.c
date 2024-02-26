f03e0837536ce665a76171d49d39df184cac4613
root <alexisoblivion@gmail.com>
2024-02-26 17:57:30 +0000

    0. 98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/*
+ * File: 0-reset_to_98.c
+ * Author: Alexis R. Rodriguez
+ */
+#include "main.h"
+/**
+  * reset_to_98 - changing the value
+  *
+  * @n: that's the pointer
+  * Return: void
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
