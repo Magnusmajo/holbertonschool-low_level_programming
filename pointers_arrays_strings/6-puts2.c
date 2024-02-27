a02ec44098f00e2cd9fe76396cf1856a183121b3
root <alexisoblivion@gmail.com>
2024-02-27 00:43:21 +0000

    6. Half the lies they tell about me arenot true

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-puts2.c
+ * Author: Alexis R. Rodriguez
+ */
+#include "main.h"
+/**
+ * puts2 - prints alternate characters
+ *
+ * @str: string to alternate print
+ *
+ * Return: always void
+ */
+void puts2(char *str)
+{
+	int counter = 0;
+
+	while (*str)
+	{
+		if (counter++ % 2 == 0)
+			_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
