4f38f7184a3dad0ec972e88d64d50c5433b80bab
root <alexisoblivion@gmail.com>
2024-02-29 15:44:31 +0000

    program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - print program name
+ *@argc: number fo arguments on my program
+ *@argv: array of arguments on my program
+ *Return: End
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
