ab50be7d921ab1da5313e492764c54ac655a82f9
root <alexisoblivion@gmail.com>
2024-03-01 14:36:49 +0000

    3.Neither irony nor sarcasm is argument

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *main - Multiplies two numbers
+ *@argc: numbers of an argument
+ *@argv: list of an array
+ *Return: Error 1, end 0
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2, mult;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	mult = n1 * n2;
+
+	printf("Result: %i\n", mult);
+
+	return (0);
+
+}
