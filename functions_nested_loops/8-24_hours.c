cdadd03858173224928c07760f36b0df5e04e43d
root <alexisoblivion@gmail.com>
2024-03-07 18:01:00 -0300

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,21 +7,21 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k, y, m, t;
+	int i, j, k, l, m, n;
 
 	for (i = 0; i < 24 ; i++)
 	{
 		k = i / 10;
-		y = i % 10;
+		l = i % 10;
 		for (j = 0; j < 60; j++)
 		{
 			m = j / 10;
-			t = j % 10;
+			n = j % 10;
 			_putchar('0' + k);
-			_putchar('0' + y);
+			_putchar('0' + l);
 			_putchar(':');
 			_putchar('0' + m);
-			_putchar('0' + t);
+			_putchar('0' + n);
 			_putchar('\n');
 		}
 	}
