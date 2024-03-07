f5c0ea5f3fa482eb31c29ce7cae55b143c56450b
root <alexisoblivion@gmail.com>
2024-03-07 16:17:57 -0300

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints 10 times the alphabet
- *
- * Return: On success 1.
+ * print_alphabet - prints the alphabet, 
+ * in lowercase, followed by a new line
+ * Return: On success 0.
  */
 void print_alphabet(void)
 {
