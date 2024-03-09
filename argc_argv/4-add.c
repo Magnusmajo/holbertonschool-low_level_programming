ba08827cba7f18d6533bd8a14ac88dee0d4325cc
root <alexisoblivion@gmail.com>
2024-03-09 14:54:35 +0000

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,24 +8,23 @@
  * @argv: array of strings
  * Return: 1 if error 0 success
  */
-
 int main(int argc, char *argv[])
 {
-	int a, b, sum;
+	int n_1, n_2, n_3, sum;
 
-	for (a = 1; a < argc; a++)
+	for (n_1 = 1; n_1 < argc; n_1++)
+	{
+		for (n_2 = 0; argv[n_1][n_2] != '\0'; n_2++)
 		{
-			for (b = 0; argv[a][b]; b++)
+			n_3 = isdigit(argv[n_1][n_2]);
+			if (n_3 == 0)
 			{
-				if (argv[a][b] < '0' || argv[a][b] > '9')
-					{
-						printf("Error\n");
-						return (1);
-					}
+				printf("Error\n");
+				return (1);
 			}
-
-			sum += atoi(argv[a]);
 		}
+		sum += atoi(argv[n_1]);
+	}
 	printf("%d\n", sum);
 	return (0);
 }
