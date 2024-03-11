1fd7fc6608b2e95ab326491f8480963bbc0da80c
root <alexisoblivion@gmail.com>
2024-03-11 19:00:26 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,38 +2,40 @@
 #include <stdlib.h>
 
 /**
- * str_concat -  concatenates two strings.
- * @s1: Entry String
- * @s2: Entry String
- * Return: NULL on failure
+ * str_concat - concatenate two strings.
+ *@s1: Entry string.
+ *@s2: Entry string.
+ *Return: newly array with content of s1 and s2
  */
 
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
-	int n_1, n_2, sum = 0, n_3;
+	int a, b, result = 0, c;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (n_1 = 0; s1[n_1] != '\0'; n_1++)
 
-	for (n_2 = 0; s2[n_2] != '\0'; n_2++)
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
 
-	sum = n_1 + n_2;
+	result = a + b;
 
-	p = malloc(sum + sizeof(char));
+	p = malloc(result + sizeof(char));
 
 	if (p == 0)
 		return (NULL);
 
-	for (n_3 = 0; n_3 < sum; n_3++)
+	for (c = 0; c < result; c++)
 	{
-		if (n_3 <= n_1 - 1)
-			p[n_3] = s1[n_3];
-		if (n_3 <= n_2)
-			p[n_3 + n_1] = s2[n_3];
+		if (c <= a - 1)
+			p[c] = s1[c];
+		if (c <= b)
+			p[c + a] = s2[c];
 	}
 
 	return (p);
