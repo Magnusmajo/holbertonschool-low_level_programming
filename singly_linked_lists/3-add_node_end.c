8d21ecf1474b101ab3ffc483c71e658c6f00994a
root <alexisoblivion@gmail.com.com>
2024-03-30 02:25:56 +0000

    New node

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -22,7 +22,6 @@ size_t _strlen(const char *s)
 * @str: Entry str
 * Return: New node
 */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
@@ -37,10 +36,22 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
+
 	new_node->len = _strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+	new_node->next = NULL;
+
+	if (!*head)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	else
+	{
+		while ((*head)->next != NULL)
+			head = &(*head)->next;
+
+		(*head)->next = new_node;
+	}
 
 	return (new_node);
-
 }
