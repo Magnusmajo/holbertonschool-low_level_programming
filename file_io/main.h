aa4183ab2197b4ecd725018cd253744029d640f6
root <alexisoblivion@gmail.com>
2024-03-31 00:12:12 +0000

    C-File i/o

---
+++ file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef FLIO
+#define FLIO
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <string.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
